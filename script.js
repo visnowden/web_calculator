023fe30860ad63044e71f22d189f8f4c4edb1976
Hacker-Bug-Hunter <Viniccamarg13@gmail.com>
2023-09-30 09:43:06 -0300

    commit to main

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let displayValue = ""
+let displayValue = "";
 
 function appendToDisplay(value) {
-    displayValue += value
-    document.getElementById("display").value = displayValue
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
 }
 
 function clearDisplay() {
-    displayValue = ""
-    document.getElementById("display").value = displayValue
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
 }
-function backspace(){
-    displayValue = displayValue.slice(0, -1)
-    document.getElementById("display").value = displayValue
+
+function backspace() {
+  displayValue = displayValue.slice(0, -1);
+  document.getElementById("display").value = displayValue;
 }
+
 function calculateResult() {
-    try {
-        displayValue = String(eval(displayValue))
-        document.getElementById("display").value = displayValue
-    } catch (error) {
-        displayValue = "Error"
-        document.getElementById("display").value = displayValue
-    }
+  try {
+    // Usar parseFloat para evitar problemas com concatenação de strings
+    displayValue = String(parseFloat(eval(displayValue).toFixed(10)));
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    displayValue = "Error";
+    document.getElementById("display").value = displayValue;
+    console.error(error); // Log do erro no console para depuração
+  }
 }
