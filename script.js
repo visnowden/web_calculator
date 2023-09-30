b2648d59cac9cee644ab45ec58375ca994da322e
Hacker-Bug-Hunter <Viniccamarg13@gmail.com>
2023-09-30 12:51:17 -0300

    Melhorando...

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-let displayValue = ""
+let displayValue = "";
 
 function appendToDisplay(value) {
-    displayValue += value
-    document.getElementById("display").value = displayValue
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
 }
 
 function clearDisplay() {
-    displayValue = ""
-    document.getElementById("display").value = displayValue
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
 }
-function backspace(){
-    displayValue = displayValue.slice(0, -1)
-    document.getElementById("display").value = displayValue
+function backspace() {
+  displayValue = displayValue.slice(0, -1);
+  document.getElementById("display").value = displayValue;
 }
 function calculateResult() {
-    try {
-        displayValue = String(eval(displayValue))
-        document.getElementById("display").value = displayValue
-    } catch (error) {
-        displayValue = "Error"
-        document.getElementById("display").value = displayValue
+  try {
+    displayValue = String(parseFloat(eval(displayValue).toFixed(10)));
+    if (displayValue == Infinity) {
+      displayValue = "Erro";
     }
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    displayValue = "Erro";
+    document.getElementById("display").value = displayValue;
+  }
 }
