2a5b0105c180c415c4d5ee076d8905ba25005188
Hacker-Bug-Hunter <Viniccamarg13@gmail.com>
2023-11-02 23:16:14 -0300

    Update script.js
    Fixing another bug, that allowed that the operator as +, -, /, *, ... could be writed next

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-let displayValue = ""
+let operations = ['/', '+', '-', '*', '.']
+let displayValue = ''
+
 function appendToDisplay(value) {
-  displayValue += value
-  document.getElementById("display").value = displayValue
-}
-function clearDisplay() {
-  displayValue = ""
-  document.getElementById("display").value = displayValue
-}
-function backspace() {
-  displayValue = displayValue.slice(0, -1)
-  document.getElementById("display").value = displayValue
-}
-function calculateResult() {
-  try {
-    displayValue = String(parseFloat(eval(displayValue).toFixed(10)))
-    if (displayValue == Infinity) {
-      displayValue = "Erro"
+    if (operations.includes(value)) {
+        if (operations.includes(displayValue.slice(-1))) {
+            displayValue = displayValue.slice(0, -1)
+        } else if (displayValue == '') {
+            value = ''
+        }
     }
-    document.getElementById("display").value = displayValue
-  } catch (error) {
-    displayValue = ""
-    document.getElementById("display").value = "Erro"
-  }
+    displayValue += value
+    document.getElementById('display').value = displayValue
+}
+
+function clearDisplay() {
+    displayValue = ''
+    document.getElementById('display').value = displayValue
+}
+
+function backspace() {
+    displayValue = displayValue.slice(0, -1)
+    document.getElementById('display').value = displayValue
+}
+
+function calculateResult() {
+    try {
+        displayValue = String(parseFloat(eval(displayValue).toFixed(3)))
+        if (displayValue == Infinity) {
+            displayValue = 'Erro'
+        }
+        document.getElementById('display').value = displayValue
+    } catch (error) {
+        displayValue = ''
+        document.getElementById('display').value = 'Erro'
+    }
 }
