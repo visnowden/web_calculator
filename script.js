e8f9344a5cafd17cb3746774805438ee53e5ed9e
Hacker-Bug-Hunter <Viniccamarg13@gmail.com>
2023-12-01 19:10:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
-let operations = ['/', '+', '-', '*', '.'];
-let displayValueSystem = '';
-let displayValue = '';
+let operations = ['/', '+', '-', '*', '.']
+let displayValueSystem = displayValue = ''
+
 function appendToDisplay(value) {
     if (operations.includes(value)) {
-        if (displayValueSystem.split(/[\*\/\+\-]/).slice(-1) % 2 == 0) {
+        if (operations.includes(displayValueSystem.slice(-1))) {
+            displayValueSystem = displayValueSystem.slice(0, -1)
+        } else if (displayValueSystem == '') {
             value = ''
         }
-        if (operations.includes(displayValueSystem.slice(-1))) {
-            displayValueSystem = displayValueSystem.slice(0, -1);
-        } else if (displayValueSystem == '') {
-            value = '';
-        }
     }
-    displayValueSystem += value;
-    userVision();
-    document.getElementById('display').value = displayValue;
+    displayValueSystem += value
+    userVision()
+    document.getElementById('display').value = displayValue
 }
 function clearDisplay() {
-    displayValueSystem = '';
-    userVision();
-    document.getElementById('display').value = 0;
+    displayValueSystem = ''
+    userVision()
+    document.getElementById('display').value = 0
 }
 function backspace() {
-    displayValueSystem = displayValueSystem.slice(0, -1);
+    displayValueSystem = displayValueSystem.slice(0, -1)
     if (displayValueSystem.length == 0) {
-        document.getElementById('display').value = 0;
+        document.getElementById('display').value = 0
     } else {
-        userVision();
-        document.getElementById('display').value = displayValue;
+        userVision()
+        document.getElementById('display').value = displayValue
     }
 }
 function calculateResult() {
-    displayValueSystem = String(parseFloat(eval(displayValueSystem).toFixed(3)));
-    userVision();
+    displayValueSystem = String(parseFloat(eval(displayValueSystem).toFixed(3)))
+    userVision()
     if (!isFinite(displayValueSystem)) {
-        displayValueSystem = '';
-        userVision();
-        document.getElementById('display').value = 'Erro';
+        displayValueSystem = ''
+        userVision()
+        document.getElementById('display').value = 'Erro'
     } else {
-        document.getElementById('display').value = displayValue;
+        document.getElementById('display').value = displayValue
     }
 }
 function userVision() {
-    displayValue = '';
+    displayValue = ''
     for (var i = 0; i < displayValueSystem.length; i++) {
         switch (displayValueSystem[i]) {
             case '.':
-                displayValue += ',';
-                break;
-            case '*':
-                displayValue += '.';
-                break;
-            case '/':
-                displayValue += '÷';
-                break;
-            case '-':
-                displayValue += '−';
-                break;
-            default:
-                displayValue += displayValueSystem[i];
-        }
+                displayValue += ','
+                break
+                case '*':
+                displayValue += '.'
+                break
+                case '/':
+                displayValue += '÷'
+                break
+                case '-':
+                displayValue += '−'
+                break
+                default:
+                displayValue += displayValueSystem[i]
+            }
     }
     if (displayValue == '') {
-        displayValue = '0';
+        displayValue = '0'
     }
 }
