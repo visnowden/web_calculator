e8abcb0e5c6bf46150eff02a3632e7fdf6d34061
visnowden <viniccamarg13@gmail.com>
2024-01-08 00:05:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,81 @@
-let operations = ['/', '+', '-', '*', '']
-let displayValueSystem = displayValue = ''
+let operations = ["/", "+", "-", "*"]
+let displayValueSystem = (displayValue = "")
 
 function appendToDisplay(value) {
-
     if (operations.includes(value)) {
         if (operations.includes(displayValueSystem.slice(-1))) {
             displayValueSystem = displayValueSystem.slice(0, -1)
+        } else if (displayValueSystem == "") {
+            value = ""
         }
     }
-
-    else if (displayValueSystem == '') {
-        value = ''
+    else if (value == "") {
+        value = ""
+    }
+    if (value == "." && displayValueSystem == "") {
+        value = "0."
+    }
+    if (!displayValueSystem && value === 0) {
+        value = ""
     }
 
-    if (!displayValueSystem & value == 0) {
-        value = ''
-    }
-    if (value == '' & !displayValueSystem.includes('')) {
-        value = '0.'
-    }
     displayValueSystem += value
+    console.log(displayValueSystem)
+    console.log(displayValueSystem.split(/[\/*+-]/))
+    console.log(displayValueSystem.split(/[\/*+-]/).filter(item => item != ''))
+    console.log(displayValueSystem.split(/[\/*+-]/).filter(item => item != '').slice(-1))
+    console.log(displayValueSystem.split(/[\/*+-]/).filter(item => item != '').slice(-1).includes("."))
+    
+    console.log("\"" + displayValueSystem + "\"")
     userVision()
-    document.getElementById('display').value = displayValue
+    document.getElementById("display").value = displayValue
 }
 function clearDisplay() {
-    displayValueSystem = ''
+    displayValueSystem = ""
     userVision()
-    document.getElementById('display').value = 0
+    document.getElementById("display").value = 0
 }
 function backspace() {
     displayValueSystem = displayValueSystem.slice(0, -1)
     if (displayValueSystem.length == 0) {
-        document.getElementById('display').value = 0
+        document.getElementById("display").value = 0
     } else {
         userVision()
-        document.getElementById('display').value = displayValue
+        document.getElementById("display").value = displayValue
     }
 }
 function calculateResult() {
     displayValueSystem = String(parseFloat(eval(displayValueSystem).toFixed(3)))
     userVision()
     if (!isFinite(displayValueSystem)) {
-        displayValueSystem = ''
+        displayValueSystem = ""
         userVision()
-        document.getElementById('display').value = 'Erro'
+        document.getElementById("display").value = "Erro"
     } else {
-        document.getElementById('display').value = displayValue
+        document.getElementById("display").value = displayValue
     }
 }
 function userVision() {
-    displayValue = ''
+    displayValue = ""
     for (var i = 0; i < displayValueSystem.length; i++) {
         switch (displayValueSystem[i]) {
-            case '':
-                displayValue += ','
+            case ".":
+                displayValue += ","
                 break
-            case '*':
-                displayValue += ''
+            case "*":
+                displayValue += "x"
                 break
-            case '/':
-                displayValue += '÷'
+            case "/":
+                displayValue += "÷"
                 break
-            case '-':
-                displayValue += '−'
+            case "-":
+                displayValue += "−"
                 break
             default:
                 displayValue += displayValueSystem[i]
         }
     }
-    if (displayValue == '') {
-        displayValue = '0'
+    if (displayValue == "") {
+        displayValue = "0"
     }
 }
